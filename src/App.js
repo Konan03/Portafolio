3357c5dfd11ed0ed495edf17ef94b9f4f14162d6
Manuel <manuelcaicedo52@gmail.com>
2023-11-04 10:11:33 -0500

    working description

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/nav/navbar';
+import Description from './components/body/description';
 
 function App() {
   return (
-    <Navbar></Navbar>
+    <>
+      <Navbar />
+      <Description />
+    </>
+    
   );
 }
 
---
+++ src/components/body/description.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../../css/description.css"
+
+function Description(){
+    return(
+        <div className="description">
+            <div>
+                <p>
+
+                </p>
+            </div>
+
+            <img></img>
+        </div>
+    );
+}
+
+export default Description
