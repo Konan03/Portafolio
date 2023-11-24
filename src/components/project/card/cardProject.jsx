ac3611a41d3d99304cc8d31bee21c37d2681f337
Manuel Caicedo <manuelcaicedo52@gmail.com>
2023-11-23 21:57:32 -0500

    add carousel for img in the cards

--- src/components/project/card/cardProject.jsx
+++ src/components/project/card/cardProject.jsx
@@ -1,25 +1,34 @@
 import React from "react";
-
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import { Avatar, Card } from 'antd';
+import { Card } from 'antd';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../../../css/cardProject.css"
 
 const { Meta } = Card;
 
-const CardProject = ({title, description, image, alt}) => (
+const CardProject = ({title, description, images, alt}) => (
   <Card
     className="cardProject"
     cover={
-      <img
-        alt={alt || image}
-        src={image}
-      />
+      <Carousel  showThumbs={false} className="carousel">
+        {images && images.length > 0 ? (
+          images.map((img, index) => (
+            <div key={index}>
+              <img src={img} alt={alt || `Imagen ${index + 1}`} />
+            </div>
+          ))
+        ) : (
+          <div>
+            <img src="path_to_default_image.png" alt="default image" />
+          </div>
+        )}
+      </Carousel>
     }
   >
     <Meta
       className="metaTitle"
       title={<span>{title}</span>}
-      description={<span >{description}</span> }
+      description={<span>{description}</span>}
     />
   </Card>
 );
--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -3,21 +3,20 @@ import CardProject from "./card/cardProject";
 import "../../css/project.css"
 import projectsData from "../project/card/projects.json"
 
-function Project(){
-    return( 
+function Project() {
+    return (
         <section className="container">
-            {projectsData.map((project) => (
+          {projectsData.map((project) => (
             <CardProject
-                key={project.id}
-                title={project.title}
-                description={project.description}
-                image={project.image}
-                className="container__card"
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              images={project.images} // Asegúrate de que esto se corresponde con tu JSON actualizado
+              alt={project.alt}
             />
-        ))}
-            
+          ))}
         </section>
-    );
+      );
 }
 
-export default Project
+export default Project;
