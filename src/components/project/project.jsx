0784f1702a9294e91b75d30ca252ab0b07940ea2
Manuel Caicedo <manuelcaicedo52@gmail.com>
2024-02-03 12:49:36 -0500

    nada por ahora

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -4,20 +4,23 @@ import "../../css/project.css"
 import projectsData from "../project/card/projects.json"
 
 function Project() {
-    return (
-        <section className="container">
-          {projectsData.map((project) => (
-            <CardProject
-              key={project.id}
-              title={project.title}
-              description={project.description}
-              images={project.images} // Asegúrate de que esto se corresponde con tu JSON actualizado
-              alt={project.alt}
-              repo={project.repo}
-            />
-          ))}
-        </section>
-      );
+
+
+  return (
+    
+    <section className="container">
+      {projectsData.map((project) => (
+        <CardProject
+          key={project.id}
+          title={project.title}
+          description={project.description}
+          images={project.images} // Asegúrate de que esto se corresponde con tu JSON actualizado
+          alt={project.alt}
+          repo={project.repo}
+        />
+      ))}
+    </section>
+  );
 }
 
 export default Project;
